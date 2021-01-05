d28e0aaa2eccff08941b2997a5f942752494159a
Eduardo Mozart de Oliveira <2974895+coldscientist@users.noreply.github.com>
2021-01-05 10:37:24 -0300

    Update sfx_build.bat

--- Scripts/Uvnc2me/sfx_build.bat
+++ Scripts/Uvnc2me/sfx_build.bat
@@ -1,11 +1,21 @@
 pushd "%~dp0"
 del /f uvnc2me_custom.7z
 bin\7zr.exe a -r uvnc2me_custom.7z uvnc2me_custom\*.*
-copy bin\7zSD.sfx "%temp%"
+copy bin\7zsd_All.sfx "%temp%"
 if exist "uvnc2me_custom\main.ico" (
-	bin\Resourcer.exe -op:upd -src:"%temp%\7zSD.sfx" -type:14 -name:1 -file:"uvnc2me_custom\main.ico"
+	bin\Resourcer.exe -op:upd -src:"%temp%\7zsd_All.sfx" -type:14 -name:101 -file:"uvnc2me_custom\main.ico"
 )
-copy /b "%temp%\7zSD.sfx" + 7zsfxbuilder_uvnc2me_custom.txt + uvnc2me_custom.7z uvnc2me_custom_sfx.exe
-del /f "%temp%\7zSD.sfx"
+copy /b "%temp%\7zsd_All.sfx" + 7zsfxbuilder_uvnc2me_custom.txt + uvnc2me_custom.7z uvnc2me_custom_sfx.exe
+del /f "%temp%\7zsd_All.sfx"
+
+setlocal
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "10.0" (
+	IE4Uinit.exe -Show
+) else (
+	IE4Uinit.exe -ClearIconCache
+)
+endlocal
+
 popd
 pause
