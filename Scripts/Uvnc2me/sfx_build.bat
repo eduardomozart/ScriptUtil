925879f1cdbea1bb1688f8273f9d756714ac1102
Eduardo Mozart de Oliveira <2974895+coldscientist@users.noreply.github.com>
2021-01-05 17:00:51 -0300

    Update sfx_build.bat

--- Scripts/Uvnc2me/sfx_build.bat
+++ Scripts/Uvnc2me/sfx_build.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
-del /f uvnc2me_custom.7z
+del /q /f uvnc2me_custom.7z
 bin\7zr.exe a -r uvnc2me_custom.7z uvnc2me_custom\*.*
 copy bin\7zsd_All.sfx "%temp%"
 if exist "uvnc2me_custom\main.ico" (
@@ -8,7 +8,7 @@ REM	bin\ResourceHacker.exe -addoverwrite "bin\7zsd_All.sfx", "bin\7zsd_All.sfx",
 	bin\rcedit-x86.exe "%temp%\7zsd_All.sfx" --set-icon "uvnc2me_custom\main.ico"
 )
 copy /b "%temp%\7zsd_All.sfx" + 7zsfxbuilder_uvnc2me_custom.txt + uvnc2me_custom.7z uvnc2me_custom_sfx.exe
-del /f "%temp%\7zsd_All.sfx"
+del /q /f "%temp%\7zsd_All.sfx"
 
 setlocal
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
