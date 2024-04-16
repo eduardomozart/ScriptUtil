0bcf0f0c9ff8f51b55e2ac2be91f463dab901a2f
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-04-16 14:19:50 -0300

    Update CiscoRAM.pm

--- Scripts/GLPI/Agent/SNMP/MibSupport/CiscoRAM.pm
+++ Scripts/GLPI/Agent/SNMP/MibSupport/CiscoRAM.pm
@@ -14,8 +14,8 @@ use constant    priority => 5;
 use constant    cisco       => '.1.3.6.1.4.1.9';
 
 # See CISCO-MEMORY-POOL
-use constant    ciscoMemoryPoolUsed     => cisco . '.9.48.1.1.1.5' . '.1';
-use constant    ciscoMemoryPoolFree     => cisco . '.9.48.1.1.1.6' . '.1';
+use constant    ciscoMemoryPoolUsed     => cisco . '.9.48.1.1.1.5';
+use constant    ciscoMemoryPoolFree     => cisco . '.9.48.1.1.1.6';
 
 our $mibSupport = [
     {
@@ -27,20 +27,39 @@ our $mibSupport = [
 sub run {
 	my ($self) = @_;
 
-    my $device = $self->device
+	my $device = $self->device
         or return;
-		
-	my $ramFree = $self->get(ciscoMemoryPoolFree);
-	# $device->{logger}->debug("Cisco Free RAM: " . $ramFree);
-	my $ramUsed = $self->get(ciscoMemoryPoolUsed);
-	# $device->{logger}->debug("Cisco Used RAM: " . $ramUsed);
+	
+	# my $ramFree = $self->get(ciscoMemoryPoolFree.'.1');
+	my $ramFree = 0;
+	my $ramsFree = $self->walk(ciscoMemoryPoolFree) || {};
+	if ($ramsFree) {
+        	foreach my $index (keys(%{$ramsFree})) {
+			$ramFree += $ramsFree->{$index};
+        	}
+	}
+	$device->{logger}->debug("Cisco Free RAM: " . $ramFree);
 	
 	# $device->{INFO}->{MEMORY} = int(($ramFree / ($ramFree + $ramUsed)) * 100);
-	$device->{INFO}->{MEMORY} = int($ramFree / (1000 * 1000));
-	# $device->{logger}->debug("Cisco MEMORY: " . $device->{INFO}->{MEMORY});
+	$device->{INFO}->{MEMORY} = int($ramFree / (1000 * 1000)) 
+		if defined($ramFree) && isInteger($ramFree);
+	$device->{logger}->debug("Cisco MEMORY: " . $device->{INFO}->{MEMORY});
 	
-	$device->{INFO}->{RAM} = sprintf "%.0f", ($ramFree + $ramUsed) / (1000 * 1000);
-	# $device->{logger}->debug("Cisco RAM: " . $device->{INFO}->{RAM});
+	if(!(defined($device->{INFO}->{RAM}))) {
+		# my $ramUsed = $self->get(ciscoMemoryPoolUsed.'.1');
+		my $ramUsed = 0;
+		my $ramsUsed = $self->walk(ciscoMemoryPoolUsed) || {};
+		if ($ramsUsed) {
+			foreach my $index (keys(%{$ramsUsed})) {
+				$ramUsed += $ramsUsed->{$index};
+			}
+		}
+		$device->{logger}->debug("Cisco Used RAM: " . $ramUsed);
+	
+		$device->{INFO}->{RAM} = sprintf "%.0f", ($ramFree + $ramUsed) / (1000 * 1000)
+			if defined($ramFree) && isInteger($ramFree) && defined($ramUsed) && isInteger($ramUsed);
+		$device->{logger}->debug("Cisco RAM: " . $device->{INFO}->{RAM});
+	}
 }
 
 1;
