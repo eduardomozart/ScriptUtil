d9918ac147a5dfcfcd9e1425b0eacc52025fac33
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-11-07 17:22:24 -0300

    Hello World!

---
+++ Scripts/DokuWiki/dw2pdf/nethouse/fonts.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'asap' => [
+        'R' => 'Asap-ExtraLight.ttf',
+        'I' => 'Asap-ExtraLightItalic.ttf'
+    ],
+    'asap-bold' => [
+        'R' => 'Asap-ExtraBold.ttf',
+        'I' => 'Asap-ExtraBoldItalic.ttf'
+    ],
+    'carlito' => [
+        'R' => 'Carlito-Regular.ttf',
+        'I' => 'Carlito-Italic.ttf'
+    ],
+    'libre-franklin' => [
+        'R' => 'LibreFranklin-Medium.ttf',
+        'I' => 'OPTIFranklinGothic-MediumIt.otf'
+    ]
+];
