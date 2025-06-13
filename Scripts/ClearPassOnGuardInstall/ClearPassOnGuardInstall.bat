cfa7718f3a76b38ba2cd52a9f6b3ab41aed47615
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-06-13 11:51:05 -0300

    Update ClearPassOnGuardInstall.bat

--- Scripts/ClearPassOnGuardInstall/ClearPassOnGuardInstall.bat
+++ Scripts/ClearPassOnGuardInstall/ClearPassOnGuardInstall.bat
@@ -13,6 +13,6 @@ if not '%~1' == '' (
 
 :: Actual work
 pushd "%~dp0"
-powershell.exe -NoProfile -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; & 'ClearPassOnGuardInstall.ps1'"
+powershell -NoProfile -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; & 'ClearPassOnGuardInstall.ps1'"
 REM powershell -ExecutionPolicy Bypass -NoProfile -File "ClearPassOnGuardInstall.ps1"
 popd
